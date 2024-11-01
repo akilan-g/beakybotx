b615f1b28abffb530eb6787ffa5800056a5fa312
akilan-g <akilan.25.g@gmail.com>
2024-11-01 15:13:55 +0100

    updated the res to from 320x240 to 640x480

--- lan-camera.py
+++ lan-camera.py
@@ -10,7 +10,7 @@ def generate_frames(): # Function to generate frames continuously for video stre
 
 #with Picamera2() as camera: # Initialize the Raspberry Pi camera using Picamera2
     camera=Picamera2(tuning="/usr/share/libcamera/ipa/rpi/vc4/imx219_noir.json")       
-    camera.preview_configuration.main.size = (320, 240)  # Set the camera resolution to 640x480 pixels
+    camera.preview_configuration.main.size = (640, 480)  # Set the camera resolution to 640x480 pixels
     camera.preview_configuration.main.format = "RGB888" # Set the camera image format to RGB888 (24-bit color)
     camera.preview_configuration.align()  # Align the camera's preview configuration (important for hardware setup)
     camera.configure("preview")  # Configure the camera to be in preview mode
